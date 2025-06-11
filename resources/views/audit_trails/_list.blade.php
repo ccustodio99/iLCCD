0781566054ca0af0f43cdb356c99cf8312d12fcc
CHRISTIAN CUSTODIO <c.custodio@lcc-daet.edu.ph>
2025-06-11 19:26:31 +0800

    [AuditTrail] Store changes on update

--- resources/views/audit_trails/_list.blade.php
+++ resources/views/audit_trails/_list.blade.php
@@ -2,10 +2,19 @@
     <h6 class="mt-3">Audit Trail</h6>
     <ul class="list-group mb-3">
         @foreach($logs as $log)
-            <li class="list-group-item d-flex justify-content-between">
-                <span>{{ $log->created_at->format('Y-m-d H:i') }}</span>
-                <span>{{ $log->user?->name ?? 'System' }}</span>
-                <span>{{ ucfirst($log->action) }}</span>
+            <li class="list-group-item">
+                <div class="d-flex justify-content-between">
+                    <span>{{ $log->created_at->format('Y-m-d H:i') }}</span>
+                    <span>{{ $log->user?->name ?? 'System' }}</span>
+                    <span>{{ ucfirst($log->action) }}</span>
+                </div>
+                @if($log->changes)
+                    <ul class="list-unstyled small mb-0 mt-1">
+                        @foreach($log->changes as $field => $values)
+                            <li>{{ $field }}: {{ $values['old'] }} → {{ $values['new'] }}</li>
+                        @endforeach
+                    </ul>
+                @endif
             </li>
         @endforeach
     </ul>
