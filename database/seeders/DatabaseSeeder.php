7334540d9733dbc8cd1a41de06e70b53c7e32f2e
CHRISTIAN CUSTODIO <c.custodio@lcc-daet.edu.ph>
2025-06-11 11:39:49 +0800

    [Demo] Add demo data seeder and update guide

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'role' => 'admin',
-            'department' => 'ITRC',
-            'is_active' => true,
-        ]);
+        // Populate demo data
+        $this->call(DemoSeeder::class);
     }
 }
