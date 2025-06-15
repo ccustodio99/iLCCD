9c0e2860dd9e02e183d0f89277b97b635286e842
CHRISTIAN CUSTODIO <c.custodio@lcc-daet.edu.ph>
2025-06-15 20:49:10 +0800

    [UI] Hide empty breadcrumbs

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,12 +1,14 @@
 @props(['links' => []])
-<nav aria-label="breadcrumb" class="mb-3">
-    <ol class="breadcrumb">
-        @foreach($links as $link)
-            @if(isset($link['url']) && $link['url'])
-                <li class="breadcrumb-item"><a href="{{ $link['url'] }}">{{ $link['label'] }}</a></li>
-            @else
-                <li class="breadcrumb-item active" aria-current="page">{{ $link['label'] }}</li>
-            @endif
-        @endforeach
-    </ol>
-</nav>
+@if(count($links))
+    <nav aria-label="breadcrumb" class="mb-3">
+        <ol class="breadcrumb">
+            @foreach($links as $link)
+                @if(isset($link['url']) && $link['url'])
+                    <li class="breadcrumb-item"><a href="{{ $link['url'] }}">{{ $link['label'] }}</a></li>
+                @else
+                    <li class="breadcrumb-item active" aria-current="page">{{ $link['label'] }}</li>
+                @endif
+            @endforeach
+        </ol>
+    </nav>
+@endif
