30db2206053ade04d06945157f63f50720ee6a2a
CHRISTIAN CUSTODIO <c.custodio@lcc-daet.edu.ph>
2025-06-16 11:58:40 +0800

    [Seeder] Improve document category seeder

--- database/seeders/DocumentCategorySeeder.php
+++ database/seeders/DocumentCategorySeeder.php
@@ -9,20 +9,8 @@ class DocumentCategorySeeder extends Seeder
 {
     public function run(): void
     {
-        $categories = [
-            'Policies & Procedures',
-            'Forms & Templates',
-            'Course Materials',
-            'Student Records',
-            'Financial & Accounting',
-            'Research & Publications',
-            'Marketing & Communications',
-            'Meeting Minutes & Reports',
-            'Archives & Historical',
-            'Miscellaneous',
-        ];
-        foreach ($categories as $name) {
-            DocumentCategory::firstOrCreate(
+        foreach (DocumentCategory::DEFAULT_CATEGORIES as $name) {
+            DocumentCategory::updateOrCreate(
                 ['name' => $name],
                 ['is_active' => true]
             );
