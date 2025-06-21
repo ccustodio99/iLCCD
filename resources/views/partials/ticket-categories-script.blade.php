c48d229fc72f63f04926c82fa74f0505d4db71dd
CHRISTIAN CUSTODIO <c.custodio@lcc-daet.edu.ph>
2025-06-21 21:37:06 +0800

    [Tickets] Simplify category data handling

--- resources/views/partials/ticket-categories-script.blade.php
+++ resources/views/partials/ticket-categories-script.blade.php
@@ -1,6 +1,10 @@
 @php
     $categoryData = $categories->mapWithKeys(function ($cat) {
-        return [$cat->id => $cat->children->map(fn ($c) => ['id' => $c->id, 'name' => $c->name])];
+        return [
+            $cat->id => $cat->children
+                ->map(fn ($c) => ['id' => $c->id, 'name' => $c->name])
+                ->values()
+        ];
     });
 @endphp
 <script>
